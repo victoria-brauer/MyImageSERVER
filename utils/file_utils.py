27fe92521a2f0ce667b6dac105f6fdccacd99e43
Victoria Brauer <torichip4@gmail.com>
2025-06-23 13:53:38 +0400

    ADDed file size check

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -9,14 +9,20 @@ MAX_FILE_SIZE = 5 * 1024 * 1024
 
 def is_allowed_file(filename: Path) -> bool:
     """Проверяем, есть ли расширение в списке разрешенных."""
-    # ext = filename.split(".")[-1]
-    # file_path = Path(filename)
     ext = filename.suffix.lower()
     print(ext)
     return ext in ALLOW_EXTENSIONS
 
 
+def is_file_size_valid(content: bytes) -> bool:
+    """Проверяем, что размер файла не превышает 5 МБ."""
+    size = len(content)
+    print(f"Размер файла: {size} байт")
+    return size <= MAX_FILE_SIZE
+
+
 def get_unique_name(filename: Path) -> str:
+    """Генерация уникального имени с сохранением расширения файла."""
     ext = filename.suffix.lower()
     unique_name = f"{uuid.uuid4().hex}{ext}"
     print(f"{unique_name=}")
