0efe6b8907d48d7854454b7ea830475f3e260bad
Victoria Brauer <torichip4@gmail.com>
2025-07-02 15:33:11 +0400

    Add logging

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,7 +1,9 @@
-import os
 from pathlib import Path
 import uuid
+import logging
 
+
+logger = logging.getLogger(__name__)
 
 ALLOW_EXTENSIONS = [".jpg", ".jpeg", ".png", ".gif"]
 MAX_FILE_SIZE = 5 * 1024 * 1024
@@ -10,14 +12,14 @@ MAX_FILE_SIZE = 5 * 1024 * 1024
 def is_allowed_file(filename: Path) -> bool:
     """Проверяем, есть ли расширение в списке разрешенных."""
     ext = filename.suffix.lower()
-    print(ext)
+    logger.debug(f"Проверка расширения файла: {ext}")
     return ext in ALLOW_EXTENSIONS
 
 
 def is_file_size_valid(content: bytes) -> bool:
     """Проверяем, что размер файла не превышает 5 МБ."""
     size = len(content)
-    print(f"Размер файла: {size} байт")
+    logger.debug(f"Размер файла: {size} байт")
     return size <= MAX_FILE_SIZE
 
 
@@ -25,5 +27,5 @@ def get_unique_name(filename: Path) -> str:
     """Генерация уникального имени с сохранением расширения файла."""
     ext = filename.suffix.lower()
     unique_name = f"{uuid.uuid4().hex}{ext}"
-    print(f"{unique_name=}")
+    logger.debug(f"Сгенерировано уникальное имя файла: {unique_name}")
     return unique_name
